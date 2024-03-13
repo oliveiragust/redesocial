56c9eea1496e0825893850cc2ae334c7f5cf882a
Casa <novousuario@Casa.local>
2024-03-12 22:28:56 -0300

    atualizando

--- app.py
+++ app.py
@@ -166,9 +166,9 @@ def create_post():
     return redirect(url_for('home', username=current_user.username))
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     return render_template('404.html'), 404
 
 
 
